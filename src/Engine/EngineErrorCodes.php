b3a12125c526c55da02b360c1e75aaa798b6a31d
tradyfox <2100742336@qq.com>
2026-07-25 01:23:19 +0800

    fix: split EngineErrorCodes out of EngineException into own file

---
+++ src/Engine/EngineErrorCodes.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Engine error codes that the gateway may return.
+ *
+ * @package OpenPoly
+ */
+
+declare(strict_types=1);
+
+namespace OpenPoly\Engine;
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Error codes that the gateway may return.
+ *
+ * @since 1.0.0-dev
+ */
+final class EngineErrorCodes {
+	// phpcs:disable Generic.NamingConventions.UpperCaseConstantName
+	public const AUTH_INVALID        = 'AUTH_INVALID';
+	public const AUTH_SITE_MISMATCH  = 'AUTH_SITE_MISMATCH';
+	public const QUOTA_EXHAUSTED     = 'QUOTA_EXHAUSTED';
+	public const RATE_LIMITED        = 'RATE_LIMITED';
+	public const ENGINE_UNAVAILABLE  = 'ENGINE_UNAVAILABLE';
+	public const INVALID_REQUEST     = 'INVALID_REQUEST';
+	public const IDEMPOTENCY_CONFLICT = 'IDEMPOTENCY_CONFLICT';
+	// phpcs:enable
+}
--- src/Engine/EngineException.php
+++ src/Engine/EngineException.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Engine error codes and exception.
+ * Engine exception, thrown when the gateway returns an error.
  *
  * @package OpenPoly
  */
@@ -10,23 +10,6 @@ declare(strict_types=1);
 namespace OpenPoly\Engine;
 
 defined( 'ABSPATH' ) || exit;
-
-/**
- * Error codes that the gateway may return.
- *
- * @since 1.0.0-dev
- */
-final class EngineErrorCodes {
-	// phpcs:disable Generic.NamingConventions.UpperCaseConstantName
-	public const AUTH_INVALID        = 'AUTH_INVALID';
-	public const AUTH_SITE_MISMATCH  = 'AUTH_SITE_MISMATCH';
-	public const QUOTA_EXHAUSTED     = 'QUOTA_EXHAUSTED';
-	public const RATE_LIMITED        = 'RATE_LIMITED';
-	public const ENGINE_UNAVAILABLE  = 'ENGINE_UNAVAILABLE';
-	public const INVALID_REQUEST     = 'INVALID_REQUEST';
-	public const IDEMPOTENCY_CONFLICT = 'IDEMPOTENCY_CONFLICT';
-	// phpcs:enable
-}
 
 /**
  * Engine exception, thrown when the gateway returns an error.
